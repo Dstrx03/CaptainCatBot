38b625fe25efd5044855c68e8361ec5e38220be1
Dstrx03 <dstrx03@gmail.com>
2020-05-04 09:44:09 +0300

    FormValueComponent improved

--- App/src/app/controls/form-value/form-value.component.ts
+++ App/src/app/controls/form-value/form-value.component.ts
@@ -8,12 +8,11 @@ import { Component, OnInit, Input } from '@angular/core';
 export class FormValueComponent implements OnInit {
 
   @Input() caption: string;
-  @Input() captionClass: string;
-  @Input() contentClass: string;
+  @Input() captionClass?: string = 'col-4';
+  @Input() contentClass?: string = 'col';
+  @Input() captionTextRight?: boolean  = true;
 
   constructor() {
-    if (this.captionClass === undefined || this.captionClass === null || this.captionClass.length === 0) this.captionClass = 'col-4';
-    if (this.contentClass === undefined || this.contentClass === null || this.contentClass.length === 0) this.contentClass = 'col';
   }
 
   ngOnInit() {
