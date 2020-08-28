0d7ebc8aa79adadc5b5ddf48547d87aabd932301
Dstrx03 <dstrx03@gmail.com>
2020-08-29 02:12:00 +0300

    ApiController made abstract

--- Cat.WebUI/Controllers/ApiController.cs
+++ Cat.WebUI/Controllers/ApiController.cs
@@ -6,7 +6,7 @@ namespace Cat.WebUI.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ApiController : ControllerBase
+    public abstract class ApiController : ControllerBase
     {
         private IMediator _mediator;
         protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
